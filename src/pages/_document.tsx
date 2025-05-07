58b18b885e1d4b161bc8517aa30657888a6aa744
RamonVBN <ramon@PC-RAMON.>
2025-05-07 19:58:55 -0300

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { getCssText } from "@/styles";
 import { Html, Head, Main, NextScript } from "next/document";
+import igniteFavicon from '../assets/Ignite simbol.png'
 
 export default function Document() {
   return (
@@ -8,6 +9,7 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
       <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+      <link rel="shortcut icon" href={igniteFavicon.src} type="image/png" />
       <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
       <body>
